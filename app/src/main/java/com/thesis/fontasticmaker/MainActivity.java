963fd06aa6a131075da38c7203026911b61a29bc
Bianca Denise del Puerto <biancadenise.delpuerto@bicol-u.edu.ph>
2016-01-22 18:28:47 +0800

    Pumupunta na sa letter B.

--- app/src/main/java/com/thesis/fontasticmaker/MainActivity.java
+++ app/src/main/java/com/thesis/fontasticmaker/MainActivity.java
@@ -33,9 +33,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
